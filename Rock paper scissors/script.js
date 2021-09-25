3befae243f87e6cc4c1a3406e588e01b7118e0fb
Kushal Tiwari <tiwarikushal057@gmail.com>
2021-09-25 10:27:52 +0545

    can't distinguish button

--- Rock paper scissors/script.js
+++ Rock paper scissors/script.js
@@ -5,26 +5,62 @@ const comScissors = document.body.querySelectorAll("button")[3];
 const comPaper = document.body.querySelectorAll("button")[4];
 const comRock = document.body.querySelectorAll("button")[5];
 
-const player = [playerRock,playerPaper,playerScissors];
-const com = [comScissors,comPaper,comRock];
-
-let playerScore = document.querySelectorAll(".number")[0].textContent;
-let comScore = document.querySelectorAll(".number")[1].textContent;
-
+let playerScore = document.querySelectorAll(".number")[0];
+let comScore = document.querySelectorAll(".number")[1];
 function random() {
     const randomIndex = Math.floor(Math.random() * 3);
     return randomIndex;
 }
 
 function rules(player, com) {
-    if(player == 0 && com == 0) {
+    if(player == 0 && com == 0 || player == 1 && com == 2 || player == 2 && com == 1 ) {
         return "player";
+    } else if(player == com) {
+        return "both";
+    } else {
+        return "com";
     }
 }
 
 function select(e) {
-    e.target.style.border = "1.7px solid white";
-    const index = random();
-    com[index].style.border = "1.7px solid white";
+    console.log(e);
+    e.style.border = "1.7px solid white";
+    const player = [];
+    let playerIndex;
+    for(i=0;i<3;i++) {
+        player[i] = document.querySelectorAll("button")[i];
+        if(player[i] == e.target) {
+            playerIndex = i;
+        }
+    }
+    const com = [];
+    for(i=5;i>2;i--) {
+        com[i] = document.querySelectorAll("button")[i];
+    }
+    console.log(playerIndex);
+    let comIndex = random();
+    com[comIndex].style.border = "1.7px solid white";
+
+    switch(rules(playerIndex, comIndex)) {
+        case "player":
+           let score = Number(playerScore.textContent);
+           score++;
+           playerScore.textContent = score;
+            break;
+        case "both":
+            let pScore = Number(playerScore.textContent);
+            pScore++;
+            playerScore.textContent = pScore;
+            let cScore = Number(comScore.textContent);
+            cScore++;
+            comScore.textContent = cScore;
+            break;
+        case "com":
+            cScore++;
+            comScore.textContent = cScore;
+    }
 }
 
+playerRock.addEventListener("click",select);
+playerScissors.addEventListener("click",select);
+playerPaper.addEventListener("click",select);
